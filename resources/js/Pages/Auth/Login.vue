972b7d1f5a7d88af590262def5f0baf3768c015e
amirmasoud <amirmasood33@gmail.com>
2021-12-12 00:00:31 +0330

    WIP

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,29 +1,19 @@
 <template>
   <Head title="Login" />
-  <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+  <div class="flex flex-col justify-center py-12 sm:px-6 lg:px-8 min-h-screen bg-gray-50">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
       <img
-        class="mx-auto h-12 w-auto"
+        class="mx-auto w-auto h-12"
         src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
         alt="Workflow"
       >
-      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+      <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900">
         Sign in to your account
       </h2>
-      <p class="mt-2 text-center text-sm text-gray-600">
-        Or
-        {{ ' ' }}
-        <a
-          href="#"
-          class="font-medium text-indigo-600 hover:text-indigo-500"
-        >
-          start your 14-day free trial
-        </a>
-      </p>
     </div>
 
-    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-      <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+    <div class="sm:mx-auto mt-8 sm:w-full sm:max-w-md">
+      <div class="py-8 px-4 sm:px-10 bg-white sm:rounded-lg shadow">
         <form
           class="space-y-6"
           action="#"
@@ -43,7 +33,7 @@
                 type="email"
                 autocomplete="email"
                 required=""
-                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                class="block py-2 px-3 w-full sm:text-sm placeholder-gray-400 rounded-md border border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm appearance-none focus:outline-none"
               >
             </div>
           </div>
@@ -62,22 +52,22 @@
                 type="password"
                 autocomplete="current-password"
                 required=""
-                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                class="block py-2 px-3 w-full sm:text-sm placeholder-gray-400 rounded-md border border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm appearance-none focus:outline-none"
               >
             </div>
           </div>
 
-          <div class="flex items-center justify-between">
+          <div class="flex justify-between items-center">
             <div class="flex items-center">
               <input
                 id="remember-me"
                 name="remember-me"
                 type="checkbox"
-                class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                class="w-4 h-4 text-indigo-600 rounded border-gray-300 focus:ring-indigo-500"
               >
               <label
                 for="remember-me"
-                class="ml-2 block text-sm text-gray-900"
+                class="block ml-2 text-sm text-gray-900"
               >
                 Remember me
               </label>
@@ -96,7 +86,7 @@
           <div>
             <button
               type="submit"
-              class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              class="flex justify-center py-2 px-4 w-full text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 rounded-md border border-transparent focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 shadow-sm focus:outline-none"
             >
               Sign in
             </button>
@@ -105,21 +95,21 @@
 
         <div class="mt-6">
           <div class="relative">
-            <div class="absolute inset-0 flex items-center">
+            <div class="flex absolute inset-0 items-center">
               <div class="w-full border-t border-gray-300" />
             </div>
-            <div class="relative flex justify-center text-sm">
-              <span class="px-2 bg-white text-gray-500">
+            <div class="flex relative justify-center text-sm">
+              <span class="px-2 text-gray-500 bg-white">
                 Or continue with
               </span>
             </div>
           </div>
 
-          <div class="mt-6 grid grid-cols-3 gap-3">
+          <div class="grid grid-cols-3 gap-3 mt-6">
             <div>
               <a
                 href="#"
-                class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                class="inline-flex justify-center py-2 px-4 w-full text-sm font-medium text-gray-500 bg-white hover:bg-gray-50 rounded-md border border-gray-300 shadow-sm"
               >
                 <span class="sr-only">Sign in with Facebook</span>
                 <Facebook class="w-5 h-5" />
@@ -129,7 +119,7 @@
             <div>
               <a
                 href="#"
-                class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                class="inline-flex justify-center py-2 px-4 w-full text-sm font-medium text-gray-500 bg-white hover:bg-gray-50 rounded-md border border-gray-300 shadow-sm"
               >
                 <span class="sr-only">Sign in with Twitter</span>
                 <Twitter class="w-5 h-5" />
@@ -139,7 +129,7 @@
             <div>
               <a
                 :href="github"
-                class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                class="inline-flex justify-center py-2 px-4 w-full text-sm font-medium text-gray-500 bg-white hover:bg-gray-50 rounded-md border border-gray-300 shadow-sm"
               >
                 <span class="sr-only">Sign in with GitHub</span>
                 <GitHub class="w-5 h-5" />
@@ -149,7 +139,7 @@
             <div>
               <a
                 :href="google"
-                class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                class="inline-flex justify-center py-2 px-4 w-full text-sm font-medium text-gray-500 bg-white hover:bg-gray-50 rounded-md border border-gray-300 shadow-sm"
               >
                 <span class="sr-only">Sign in with Google</span>
                 <Google class="w-5 h-5" />
